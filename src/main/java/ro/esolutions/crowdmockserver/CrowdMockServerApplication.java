a746ac645478be8ff00ce44c60d63fd91890f17d
Ioan-Cristian CÎRSTEA <ioan.cirstea@esolutions.ro>
2022-08-09 16:21:49 +0300

    Initial commit

---
+++ src/main/java/ro/esolutions/crowdmockserver/CrowdMockServerApplication.java
@@ -0,0 +1,13 @@
+package ro.esolutions.crowdmockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdMockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdMockServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/esolutions/crowdmockserver/CrowdMockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.esolutions.crowdmockserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdMockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
